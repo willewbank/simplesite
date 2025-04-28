a8ca8e6bb327da2a9df8b995d55b5a90d2e313a8
Will Ewbank <william.ewbank94@gmail.com>
2025-04-28 08:11:41 +0100

    test

--- app.py
+++ app.py
@@ -1,28 +1,56 @@
 from flask import Flask, request, render_template_string
+import random
 
 app = Flask(__name__)
 
-HTML_TEMPLATE = """
+INDEX_TEMPLATE = """
 <!DOCTYPE html>
 <html>
 <head>
-    <title>Background Color Changer</title>
-    <link rel="stylesheet" href="{{ url_for('static', filename='styles.css') }}">
+    <title>Colour guesser</title>
 </head>
-<body style="background-color: {{ color }};">
-    <h1>Choose a background color</h1>
-    <form method="post">
-        <input type="text" name="color" placeholder="Enter a color (e.g., red, yellow, green)" required>
-        <button type="submit">Change Color</button>
-    </form>
+<body>
+<h1>Your favorite colour guesser</h1>
+<p>Enter your name and your favorite colour will show.</p>
+<form action="/colour">
+  <label for="name">Name:</label>
+  <input type="text" id="name" name="name"><br><br>
+  <input type="submit" value="Submit">
+</form>
 </body>
 </html>
 """
 
+COLOUR_TEMPLATE = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Colour guesser</title>
+</head>
+<body style="background-color: {{ colour }};">
+<h1>Your favorite colour is {{colour}}</h1>
+<p>Is that right?</p>
+<form action="/colour">
+  <input type="radio" id="yes" name="correct" value="Yes" checked="checked">
+  <label for="html">Yes</label><br>
+  <input type="radio" id="no" name="correct" value="No">
+  <label for="css">No</label><br>
+  <input type="submit" value="Submit">
+</form>
+</body>
+</html>
+"""
+
+colours = ['red', 'yellow', 'green', 'blue', 'orange']
+
 @app.route("/", methods=["GET", "POST"])
 def index():
-    color = request.form.get("color", "clear")
-    return render_template_string(HTML_TEMPLATE, color=color)
+    return render_template_string(INDEX_TEMPLATE)
+
+@app.route("/colour", methods=["GET", "POST"])
+def index():
+    colour = random.choice(colours)
+    return render_template_string(COLOUR_TEMPLATE, colour=colour)
 
 if __name__ == "__main__":
     app.run(debug=True)
