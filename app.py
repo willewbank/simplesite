cc64e2e75bf6dc6ce9e56dbd1d6d57934d55e3b7
Will Ewbank <william.ewbank94@gmail.com>
2025-05-01 09:15:37 +0100

    no second guess

--- app.py
+++ app.py
@@ -33,22 +33,23 @@ COLOUR_TEMPLATE = """
 </head>
 <body style="background-color: {{ colour }};">
 <h1>{{name}}'s favorite colour is {{colour}}</h1>
-<p>Is that right?</p>
-<form action="/colour">
-    <div style="display: flex; gap: 10px;">
-        <div>
-            <input type="radio" id="yes" name="correct" value="Yes" checked="checked">
-            <label for="yes">Yes</label><br>
-        </div>
-        <div>
-            <input type="radio" id="no" name="correct" value="No">
-            <label for="no">No</label><br>
-        </div>
-  <input type="submit" value="Submit">
-</form>
 </body>
 </html>
 """
+
+# <p>Is that right?</p>
+# <form action="/colour">
+#     <div style="display: flex; gap: 10px;">
+#         <div>
+#             <input type="radio" id="yes" name="correct" value="Yes" checked="checked">
+#             <label for="yes">Yes</label><br>
+#         </div>
+#         <div>
+#             <input type="radio" id="no" name="correct" value="No">
+#             <label for="no">No</label><br>
+#         </div>
+#   <input type="submit" value="Submit">
+# </form>
 
 colours = ['red', 'yellow', 'green', 'blue', 'orange', 'pink', 'purple']
 
@@ -59,13 +60,14 @@ def index():
 @app.route("/colour", methods=["GET", "POST"])
 def colour():
     name = request.args.get("name", "Guest")
-    if request.method == "POST":
-        user_choice = request.form.get("correct")
-        if user_choice == "No":
-            session["colour"] = random.choice(colours)
-    if "colour" not in session:
-        session["colour"] = random.choice(colours)
-    return render_template_string(COLOUR_TEMPLATE, colour=session["colour"], name=name)
+    # if request.method == "POST":
+    #     user_choice = request.form.get("correct")
+    #     if user_choice == "No":
+    #         session["colour"] = random.choice(colours)
+    # if "colour" not in session:
+    #     session["colour"] = random.choice(colours)
+    colour = random.choice(colours)
+    return render_template_string(COLOUR_TEMPLATE, colour=colour, name=name)
 
 if __name__ == "__main__":
     app.run(debug=True)
