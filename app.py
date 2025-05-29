0e66c487cc38ecc45f89f7b277fccc14e7ed9716
Will Ewbank <121037211+willewbank@users.noreply.github.com>
2025-05-29 10:00:11 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template_string, session
+from flask import Flask, request, render_template_string, session, redirect
 import random
 
 app = Flask(__name__)
@@ -13,7 +13,7 @@ INDEX_TEMPLATE = """
 <body>
 <h1>Your favorite colour guesser</h1>
 <p>Enter your name and your favorite colour will show.</p>
-<form action="/colour">
+<form action="/colour" method="post">
   <label for="name">Name:</label>
   <input type="text" id="name" name="name"><br><br>
   <input type="submit" value="Submit">
@@ -31,9 +31,13 @@ COLOUR_TEMPLATE = """
 </head>
 <body style="background-color: {{ colour }};">
 <h1>{{name}}'s favorite colour is {{colour}}</h1>
-<p>Did I get that right? Hit try again to go back to the start.</p>
+<p>Did I get that right? Hit try again if I got it wrong.</p>
 <form action="/colour">
     <input type="submit" value="Try Again">
+</form>
+<p>Do you want to sign up to coding club?</p>
+<form action="/signup">
+    <input type="submit" value="Sign up">
 </form>
 </body>
 </html>
@@ -47,9 +51,13 @@ def index():
 
 @app.route("/colour", methods=["GET", "POST"])
 def colour():
-    name = request.args.get("name", "Guest")
+    name = request.form.get("name", "Guest")
     colour = random.choice(colours)
     return render_template_string(COLOUR_TEMPLATE, colour=colour, name=name)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return redirect("https://forms.cloud.microsoft/pages/responsepage.aspx?id=PKjhf7v5f0auHigHBSUJQdiq9VVxg5BFnGDGMD0e-41UNEpHN1FDVTZPTUtQQ1RCNFg5SVlaUzVSRy4u&route=shorturl")
 
 if __name__ == "__main__":
     app.run(debug=True)
